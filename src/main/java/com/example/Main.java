9bc6c394af109802deaa7b02a9caf8377ae289ea
nannyparra-lang <nannyparra@gmail.com>
2026-08-23 19:42:59 -0500

    terminado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -10,9 +10,14 @@ public class Main {
         BuqueCarga buque = new BuqueCarga("555", 20, 30, 50);
         camion.setCombustible(-20);
         System.out.println("El combustible del camion es: " + camion.getCombustible());
-
+        
         ArrayList<Transporte> flota = new ArrayList <>();
-        flota.add(buque);
         flota.add(camion);
+        flota.add(buque);
+        for( int i = 0; i < flota.size(); i++) {
+            Transporte transporte = flota.get(i);
+            transporte.viajar(100);
+            System.out.println("ID: " + transporte.getIdTrasporte() + "combustible restante" + transporte.getCombustible());
+        }
     }
 }
